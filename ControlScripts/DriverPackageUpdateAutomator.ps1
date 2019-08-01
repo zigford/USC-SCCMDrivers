15d666335378e2b358a223a65e75114b0d2a82e8
Jesse Harris <zigford@gmail.com>
2019-08-01 16:09:21 +1000

    Added tests to find bug in Get-ModelFromFileName
    Moved around code in DriverPackageUpdateAutomator in order to be able to
    . source the script without running anything for the purposes of
    executing pester tests over it.

--- ControlScripts/DriverPackageUpdateAutomator.ps1
+++ ControlScripts/DriverPackageUpdateAutomator.ps1
@@ -1,6 +1,8 @@
 ﻿[CmdletBinding()]
 Param()
 
+$isDotSourced = $MyInvocation.InvocationName -eq '.' -or $MyInvocation.Line -eq ''
+
 function Test-CurrentAdminRights {
     #Return $True if process has admin rights, otherwise $False
     $user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -51,44 +53,48 @@ function Get-ArchFromFileName {
     return $SplitName[0]
 }
 
-$General = '\\usc.internal\usc\appdev\General'
-$LogPath = "$General\Logs"
-$ModulePath = "$General\SCCMTools\Scripts\Modules\Dev"
-Import-Module "$ModulePath\USC-SCCMDrivers"
-$CompletedRoot = "\\usc.internal\usc\appdev\General\Packaging\CompletedPackages"
+If (!$isDotSourced) {
+    # Init Variables
+    $General = '\\usc.internal\usc\appdev\General'
+    $LogPath = "$General\Logs"
+    $ModulePath = "$General\SCCMTools\Scripts\Modules\Dev"
+    Import-Module "$ModulePath\USC-SCCMDrivers"
+    $CompletedRoot = "\\usc.internal\usc\appdev\General\Packaging\CompletedPackages"
+    $LogFile="$LogPath\PackageDriverAutomator.log"
 
-$LogFile="$LogPath\PackageDriverAutomator.log"
-If ((Test-Path $LogFile) -and (Get-Item $LogFile).Length -gt 2097152) {
-    Start-Transcript -Path $LogPath\PackageDriverAutomator.log
-} else {
-    Start-Transcript -Path $LogPath\PackageDriverAutomator.log -Append
-}
-$DriverPackageDir = "$General\Packaging\DriverPackages"
-$DriverPackages = Get-ChildItem -Path $DriverPackageDir |
-Where-Object { $_.Extension -in '.cab','.zip' }
-$ErrorActionPreference = 'Stop'
+    If ((Test-Path $LogFile) -and (Get-Item $LogFile).Length -gt 2097152) {
+        Start-Transcript -Path $LogPath\PackageDriverAutomator.log
+    } else {
+        Start-Transcript -Path $LogPath\PackageDriverAutomator.log -Append
+    }
 
-ForEach ($DriverPackage in $DriverPackages) {
-    Write-Verbose "Working on $DriverPackage"
-    $Architecture = Get-ArchFromFileName $DriverPackage
-    $Model = Get-ModelFromFileName $DriverPackage
-    $UpdateParams = @{
-        Model = $Model
-        DriverCab = $DriverPackage.FullName
-        Architecture = $Architecture
+    $DriverPackageDir = "$General\Packaging\DriverPackages"
+    $DriverPackages = Get-ChildItem -Path $DriverPackageDir |
+    Where-Object { $_.Extension -in '.cab','.zip' }
+    $ErrorActionPreference = 'Stop'
+
+    ForEach ($DriverPackage in $DriverPackages) {
+        Write-Verbose "Working on $DriverPackage"
+        $Architecture = Get-ArchFromFileName $DriverPackage
+        $Model = Get-ModelFromFileName $DriverPackage
+        $UpdateParams = @{
+            Model = $Model
+            DriverCab = $DriverPackage.FullName
+            Architecture = $Architecture
+        }
+        Update-CfgDriverPackage @UpdateParams -Verbose
+        $email = @{
+            Message = "$DriverPackage has been updated! Check the logs at $(
+                )General\Logs\PackageDriverAutomator.log"
+                #EmailAddress = 'jpharris@usc.edu.au'
+            EmailAddress = '330c4da0.usceduau.onmicrosoft.com@apac.teams.ms'
+            #EmailAddress = '3b7f44bd.usceduau.onmicrosoft.com@apac.teams.ms'
+            Subject = $DriverPackage
+        }
+        Send-EmailMessage @email
+        Write-Verbose "Moving source files to complete folder."
+        If ((pwd).path -notmatch 'c:\\') {Set-Location $env:WinDir; Write-Verbose "Had to update location to C:"}
+        Move-Item $DriverPackage.FullName $CompletedRoot -Force
     }
-    Update-CfgDriverPackage @UpdateParams -Verbose
-    $email = @{
-        Message = "$DriverPackage has been updated! Check the logs at $(
-            )General\Logs\PackageDriverAutomator.log"
-            #EmailAddress = 'jpharris@usc.edu.au'
-        EmailAddress = '330c4da0.usceduau.onmicrosoft.com@apac.teams.ms'
-        #EmailAddress = '3b7f44bd.usceduau.onmicrosoft.com@apac.teams.ms'
-        Subject = $DriverPackage
-    }
-    Send-EmailMessage @email
-    Write-Verbose "Moving source files to complete folder."
-    If ((pwd).path -notmatch 'c:\\') {Set-Location $env:WinDir; Write-Verbose "Had to update location to C:"}
-    Move-Item $DriverPackage.FullName $CompletedRoot -Force
+    Stop-Transcript
 }
-Stop-Transcript
---
+++ Tests/ControlScripts.test.ps1
@@ -0,0 +1,55 @@
+Import-Module Pester
+$TestRoot = Split-Path -Path $PSScriptRoot -Parent
+$ControlScripts = Join-Path -Path $TestRoot -ChildPath 'ControlScripts'
+$DriverPackageUpdateAutomator = Join-Path -Path $ControlScripts -ChildPath `
+    'DriverPackageUpdateAutomator.ps1'
+
+# Dot Source the script we are testing
+ 
+If (-Not (Test-Path -Path $DriverPackageUpdateAutomator)) {
+    Write-Error "Could not locate script we are testing"
+}
+
+. "$DriverPackageUpdateAutomator"
+
+# Tests
+
+Describe "Get-ModelFromFileName" {
+    $FileNameTests = [PSCustomObject]@{
+        FileName = "Win10x64-Latitude 3380-A11.cab"
+        Expected = "Latitude 3380"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude 5300 2-in-1-A00.cab"
+        Expected = "Latitude 5300 2-in-1"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude 5300 2-IN-1-A01.cab"
+        Expected = "Latitude 5300 2-IN-1"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude 7280-A11.cab"
+        Expected = "Latitude 7280"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude 7390 2-in-1-A08.cab"
+        Expected = "Latitude 7390 2-in-1"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude 7400-A00.cab"
+        Expected = "Latitude 7400"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-Latitude E7470-A12.cab"
+        Expected = "Latitude E7470"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-OptiPlex 7460 AIO-A05.cab"
+        Expected = "OptiPlex 7460 AIO"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-XPS 13 9365-A12.cab"
+        Expected = "XPS 13 9365"
+    },[PSCustomObject]@{
+        FileName = "Win10x64-XPS 13 9380-A03.cab"
+        Expected = "XPS 13 9380"
+    }
+    It "returns the device model from the file object of a dell cab pack" {
+        ForEach ($Test in $FileNameTests) {
+            Get-ModelFromFileName -FileName ([System.IO.FileInfo]$Test.FileName) |
+            Should Be $Test.Expected
+        }
+    }
+}
